9a687880ca3337f1e606272c3516340150c53462
Iurii Perederii <uyuddar@gmail.com>
2022-03-25 18:30:14 +0400

    Fix for responsiveness of the photo

--- site/src/header/index.tsx
+++ site/src/header/index.tsx
@@ -3,11 +3,15 @@ import './header.scss';
 
 export const Header: FC = () => {
     return (
-        <div className="header">
-            <img src="photo.jpg" className="rounded-circle me-4 my-2 d-none d-sm-inline" alt="photo"/>
-            <div className="d-flex flex-column justify-content-center d-sm-block">
-                <h1>Iurii Perederii</h1>
-                <h3>Senior Software Engineer</h3>
+        <div className="container-fluid d-flex flex-column justify-content-center d-sm-block header">
+            <div className="row justify-content-center px-2">
+                <div className="col col-12 col-lg-6 d-flex justify-content-sm-center">
+                    <img src="photo.jpg" className="rounded-circle me-4 d-none d-sm-block" width={95} height={95} alt="photo"/>
+                    <div>
+                        <h1>Iurii Perederii</h1>
+                        <h3>Senior Software Engineer</h3>
+                    </div>
+                </div>
             </div>
         </div>
     );
