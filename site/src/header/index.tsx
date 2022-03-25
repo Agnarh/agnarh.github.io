e1289fc96056d2d159a65dcd85b66d07d6c471ea
Iurii Perederii <uyuddar@gmail.com>
2022-03-25 19:17:36 +0400

    Adding paddings for the photo

--- site/src/header/index.tsx
+++ site/src/header/index.tsx
@@ -6,7 +6,7 @@ export const Header: FC = () => {
         <div className="container-fluid d-flex flex-column justify-content-center d-sm-block header">
             <div className="row justify-content-center px-2">
                 <div className="col col-12 col-lg-6 d-flex justify-content-sm-center">
-                    <img src="photo.jpg" className="rounded-circle me-4 d-none d-sm-block" width={95} height={95} alt="photo"/>
+                    <img src="photo.jpg" className="rounded-circle me-4 mt-1 d-none d-sm-block" width={92} height={92} alt="photo"/>
                     <div>
                         <h1>Iurii Perederii</h1>
                         <h3>Senior Software Engineer</h3>
