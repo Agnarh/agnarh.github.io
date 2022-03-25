d22babd0531aa442671c4486664346f33f3b1431
Iurii Perederii <uyuddar@gmail.com>
2022-03-25 19:00:12 +0400

    Adding default value for className in SectionWrapper

--- site/src/common/sideSectionWrapper.tsx
+++ site/src/common/sideSectionWrapper.tsx
@@ -6,7 +6,7 @@ type SectionWrapperProps = {
     children: React.ReactNode;
 };
 
-export const SectionWrapper: FC<SectionWrapperProps> = ({ name, children, className }) => (
+export const SectionWrapper: FC<SectionWrapperProps> = ({ name, children, className = '' }) => (
     <div className={`row gy-1 px-2 py-3 ${className}`}>
         <h4 className="fw-bold">{name}</h4>
         <hr/>
